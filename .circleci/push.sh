0013ea6191e9e3837f0f430cd1f3081774f7a174
Stephen Mele <Smelecs@gmail.com>
2018-07-02 16:58:04 -0400

    Fixing shell script
    Signed-off-by: Stephen Mele <Smelecs@gmail.com>

--- .circleci/push.sh
+++ .circleci/push.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-DOCKER_TAG=$0
-DOCKER_TAG_COMPONENT=$1
+DOCKER_TAG=$1
+DOCKER_TAG_COMPONENT=$2
 
 DOCKER_REPO=${DOCKER_USER}/${DOCKER_TAG_COMPONENT}
 
 if [[ -z "${CIRCLE_BRANCH}" ]]; then
+    echo "This is a CI branch build"
     DOCKER_RELEASE_TAG=${CIRCLE_BRANCH}-latest
     DOCKER_LATEST_TAG=${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}
+    echo "Tag will be: ${DOCKER_RELEASE_TAG}"
 else
     DOCKER_RELEASE_TAG=$(echo $CIRCLE_TAG | cut -d "v" -f 2)
     DOCKER_LATEST_TAG=latest
 fi
+
+echo "Version Tag will be: ${DOCKER_RELEASE_TAG}"
+echo "Latest Tag will be: ${DOCKER_LATEST_TAG}"
 
 echo "pushing and tagging image"
 docker tag ${DOCKER_TAG} ${DOCKER_REPO}:${DOCKER_RELEASE_TAG}
